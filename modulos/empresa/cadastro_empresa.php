5db7571d791694964b0dc357671e908becfeac2f
JuniorT <JuniorT@LIOMAR>
2016-06-08 21:49:47 -0300

    Correções.

--- modulos/empresa/cadastro_empresa.php
+++ modulos/empresa/cadastro_empresa.php
@@ -8,24 +8,6 @@ $frm = new TForm('Cadastro de Empresa', 530, 1000);
 
 EmpresaDAO::executeSql("set names utf8"); // configurando acentuação no mysql
 
-//$frm->addHiddenField('id');
-
-
-//$frm->setColumns(100); // define a primeira coluna do formulï¿½rio para 100 px
-// define a largura das colunas verticais do formulario para alinhamento dos campos 
-//$frm->setColumns(array(100,100,100)); 
-//$frm->setColumns(array(100,100)); 
-
-
-
-//$pc = $frm->addPageControl('pc', null, null, null, 'abaClick()');
-//$page = $pc->addPage('Cadastro', true, true, 'abaCadastro');
-//$page->setColorHighlightBackground('#FDFCD7'); // cor de fundo do campo que possuir dica ( hint )
-//$frm->addDateField('data_cadastro', 'Data Cadastro:', true, false, date("d/m/Y")); //->addEvent('onblur','sai(this)');
-
-
-
-
 $frm->addTextField('codigo_empresa','Código:',10,false,10,null,null,null,null,true)->addEvent('onblur','buscaEmpresa(this)')->addEvent('onFocus','novo()');
 
 $frm->setOnlineSearch('codigo_empresa','empresa'
@@ -61,36 +43,18 @@ $frm->addCpfCnpjField('cnpj','CNPJ:',true,null,true)->setCss('font-size','14px')
 $frm->addTextField('ie','Inscrição Estadual:',18,true)->setCss('font-size','14px')->addEvent('onkeyup', 'maskIe(this)');
 $frm->addSelectField('situacao', 'Situação:', false, 'A=Ativa,I=Inativa', false, null, null, null, null, null, null, 'A')->setCss('font-size','14px');
 
-//$frm->addButtonAjax('Salvar', null, 'antesSalvar', 'depoisSalvar', 'salvar', 'text', false, null, 'btnSalvar',false);
-// $frm->addButton('Novo', null, 'btnNovo', 'novo()');
 
-          
-
-
-//$page = $pc->addPage('Pesquisar Empresa', false, true, 'abaEmpresa');
-//$page->setColumns(100); // define a primeira coluna do formulï¿½rio da aba para 80 px
-// o atributo noclear evita que a funï¿½ï¿½o fwClearFields limpe o campo
-
-
-//$frm->addTextField('psq_razao_empresa', 'Localizar por Nome:', 40, false)->setAttribute('noclear', 'true')->setTooltip('Pesquisar - Informe o nome ou parte do nome e clique no botï¿½o Pesquisar!')->addEvent('onblur','fwClearChildFields()');
-//$frm->addButton('Pesquisar', null, 'btnPesquisar', 'atualizarGride()', null, true, false);
-//$frm->addHtmlField('html_gride');
 
 $frm->closeGroup(); // fim das abas
 
 $frm->processAction();
 
-//$frm->addButton('Alterar', null, 'btnNovo', 'salvar()', null, null, null, 'editar.gif');
-//$frm->addButtonAjax('Incluir', null, null,'novo', null, null,'text', true, false,'btnnovo')->setCss('font-size','24px');
+
 $frm->addButtonAjax('Incluir',null,null,'novo','novo','Novo...','text',false,null,'btnNovo',null,'fwSave.png','fwSave.png','fwSave.png')->setCss('font-size','24px');
 $frm->addButtonAjax('Alterar',null,'antesSalvar','depoisSalvar','salvar','Salvando...','text',false,null,'btnSalvar',null,'fwSave.png','fwSave.png','editar.gif')->setCss('font-size','24px');
 $frm->addButton('Excluir', null, 'btnCancelar', 'grideCancelar()', null, null, null, 'lixeira.gif');
-//$frm->addButtonAjax('Excluir', null, 'btnCancelar', 'grideCancelar()', null, null, null, 'lixeira.gif');
-
 
 $frm->show();
-
-
 
 
 
@@ -102,40 +66,6 @@ $frm->show();
     
 
 
-    //jQuery("#data_cadastro").attr('readonly', 'true');
-
-    //jQuery("#Novo").attr('disabled', 'true');
-
-    function pegaNomeConvenio(e)
-    {
-        //fwAtualizarCampos('nome_convenio', jQuery("#cod_convenio option:selected").text());
-        
-    }
-
-    function pegaNomeCirurgia(e)
-    {
-        //fwAtualizarCampos('desc_cirurgia_principal', jQuery("#cod_cirurgia_principal option:selected").text());
-        
-    }
-
-    function pegaNomeCirurgiao(e)
-    {
-        //fwAtualizarCampos('nome_cirurgiao_principal', jQuery("#cod_cirurgiao_principal option:selected").text());
-        
-    }
-
-    function pegaNomeEspecialidade(e)
-    {
-        //fwAtualizarCampos('desc_especialidade', jQuery("#cod_especialidade option:selected").text());
-        
-    }
-
-    function sai(e)
-    {
-        //alert(jQuery("#data_cadastro").val());
-        
-
-    }
 	
 	function upperCase(obj)
 	{
@@ -204,36 +134,6 @@ $frm->show();
        // fwSetFocus('codigo_empresa');
     }
 
-   /* function abaClick(pc, aba, id)
-    {
-        if (id == 'abaCadastro')
-        {
-
-            if (jQuery("#psq_razao_empresa").val() != '')
-            {
-                atualizarGride();
-            }
-
-        }
-
-        if (id == 'abaEmpresa')
-        {
-           
-            jQuery("#Salvar").attr('disabled', 'disabled');
-             $("#Salvar").css("display", "none");
-             $("#btnSalvar").hide("slow");
-             fwSetFocus('psq_cid');
-
-        }
-
-
-    }
-    */
-    function atualizarGride()
-    {
-        fwGetGrid('empresa/cadastro_empresa.php', 'html_gride', {"action": "criar_gride", "psq_razao_empresa": ""});
-        
-    }
     
     function buscaEmpresa(campoChave, valorChave)
     {
