c266866673738601b84d511c388d14d5f3a97278
JuniorT <JuniorT@LIOMAR>
2016-07-23 14:33:30 -0300

    correções de inclusão conselho

--- modulos/Atendimento/cad_conselho/action/salvar.php
+++ modulos/Atendimento/cad_conselho/action/salvar.php
@@ -1,22 +1,18 @@
 <?php
 
 echo "teste";
-/*
+
 $vo = new ConselhoVO();
 $vo1 = new Conselho_ufVO();
 $frm->setVo($vo);
-if (empty(ConselhoDAO::insert($vo))){
-    echo "não foi possível inserir os dados";
-}
-else{
-$dados = ConselhoDAO::selectEspecifico('select max(codigo) from conselho');  
-    var_dump($dados);
+ConselhoDAO::insert($vo);
 
+$dados = ConselhoDAO::selectEspecifico('select max(codigo)as codigo from conselho');  
 
-$vo1->setConselho($dados[0]);
+$vo1->setConselho($dados[CODIGO][0]);
 $vo1->setUf($frm->get('uf'));
 Conselho_ufDAO::insert($vo1);
-}
+echo "Dados gravados com sucesso";
 
- */
+
 ?>
